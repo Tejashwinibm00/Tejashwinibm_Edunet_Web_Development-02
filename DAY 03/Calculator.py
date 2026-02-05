eb5c54d02cee16a77ee9f0bd837f48dcd768a71d
TEJASHWINI B M <tejashwinibmtejashwinibm@gmail.com>
2026-02-05 10:11:16 +0530

    Add files via upload

---
+++ DAY 03/Calculator.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a + b
+def sub(a,b):
+    return a - b
+def mult(a,b):
+    return a * b
+def div(a,b):
+    return a / b
+
+print(add(2,4))
+print(sub(10,3))
+print(mult(2,4))
+print(div(0,-0))
